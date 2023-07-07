1f1cdd60bf24935e796990845e3eb2ab7548ca0e
akuldeepj <akuldeepj@gmail.com>
2023-07-07 05:53:29 +0530

    added major functions

--- functions.py
+++ functions.py
@@ -15,14 +15,6 @@ def create_chatid(user1, user2):
     else:
         return user2[2:] + user1[2:]
     
-def print_chat(id,filename = 'test.json'):
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    for i in data:
-        if i['id'] == id:
-            for j in i['msg']:
-                print(i['msg'][j]['id'] + ' : ' + i['msg'][j]['msg'])
-
 def verify_id(user1,user2,filename='test.json'):
     with open(filename, 'r') as f:
         data = json.load(f)
@@ -33,9 +25,81 @@ def verify_id(user1,user2,filename='test.json'):
         else:
             return False
 
-if(verify_id('u1','u2')):
-    print_chat("001002")
-else:
-    create_chatid('u1','u2')
+# if(verify_id('u1','u2')):
+#     print_chat("001002")
+# else:
+#     create_chatid('u1','u2')
 
-print(user_id())
+# print(user_id())
+
+def create_chatroom(user1, user2, filename='test.json'):
+    chat_id = create_chatid(user1['id'], user2['id'])
+    chatroom = {'id': chat_id, 'members': [user1['id'], user2['id']], 'messages': []}
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    data.append(chatroom)
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+    print(f'Chatroom {chat_id} created successfully.')
+
+def join_chatroom(user, chat_id, filename='test.json'):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    for chatroom in data:
+        if chatroom['id'] == chat_id:
+            chatroom['members'].append(user['id'])
+            with open(filename, 'w') as f:
+                json.dump(data, f)
+            print(f'User {user["id"]} joined chatroom {chat_id}.')
+            return
+    print(f'Chatroom {chat_id} not found.')
+
+def print_chat(chat_id, filename='test.json'):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    for chatroom in data:
+        if chatroom['id'] == chat_id:
+            for message in chatroom['messages']:
+                print(f'{message["id"]} : {message["msg"]}')
+
+
+from datetime import datetime
+
+def send_message(user, chat_id, message, filename='test.json'):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    for chatroom in data:
+        if chatroom['id'] == chat_id:
+            if 'messages' not in chatroom:
+                chatroom['messages'] = []
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            new_message = {'id': user['id'], 'msg': message, 'timestamp': timestamp}
+            chatroom['messages'].append(new_message)
+            with open(filename, 'w') as f:
+                json.dump(data, f)
+            print(f'Message sent by {user["id"]} in chatroom {chat_id}.')
+            return
+    print(f'Chatroom {chat_id} not found.')
+
+
+def receive_messages(chat_id, filename='test.json'):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    for chatroom in data:
+        if chatroom['id'] == chat_id:
+            for message in chatroom['messages']:
+                print(f'{message["id"]} : {message["msg"]} - {message["timestamp"]}')
+
+
+user1 = {'id': 'ID001'}
+user2 = {'id': 'ID002'}
+chat_id = create_chatid(user1['id'], user2['id'])
+create_chatroom(user1, user2)
+
+send_message(user1, chat_id, 'Hello, User2!')
+
+send_message(user2, chat_id, 'Hi, User1!')
+
+print_chat(chat_id)
+
+receive_messages(chat_id)
